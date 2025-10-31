3cc3cafda0dee1a21eac7bb49928f3db30f4d5c8
michael.nielsen <gudmundseth.dk@gmail.com>
2025-10-31 16:13:57 +0100

    parsing responseentity as Map and JsonNode

--- src/main/java/com/gudmumic/spring/rest/template/client/BeerClientImpl.java
+++ src/main/java/com/gudmumic/spring/rest/template/client/BeerClientImpl.java
@@ -1,12 +1,14 @@
 package com.gudmumic.spring.rest.template.client;
 
 import com.gudmumic.spring.rest.template.model.BeerDTO;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.boot.restclient.RestTemplateBuilder;
+import tools.jackson.databind.JsonNode;
 
 @RequiredArgsConstructor
 @Service
@@ -14,12 +16,30 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;;
 
+    private static final String baseUrl = "http://localhost:8080";
+    private static final String beerPth = "/api/v1/beer";
+
     @Override
     public Page<BeerDTO> getBeerList() {
         RestTemplate restTemplate = restTemplateBuilder.build();
+
         ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+                restTemplate.getForEntity(baseUrl + beerPth, String.class);
+
+        ResponseEntity<Map> mapResponseEntity =
+                restTemplate.getForEntity(baseUrl + beerPth, Map.class);
+
+        ResponseEntity<JsonNode> jsonNodeResponseEntity =
+                restTemplate.getForEntity(baseUrl + beerPth, JsonNode.class);
+
+        jsonNodeResponseEntity.getBody().findPath("content")
+            .forEach(beer -> {
+                  System.out.println("Beer name; " + beer.get("name"));
+                  System.out.println("Beer price; " + beer.get("price"));
+                });
+
         System.out.println(stringResponse.getBody());
+
         return null;
     }
 }
